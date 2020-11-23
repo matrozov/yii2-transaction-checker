b9ca2bfad5395f662b625411e91567be3e98bba4
Matrozov <oleg.matrozov@gmail.com>
2020-11-23 20:42:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Transaction checker
+
+Component for checking the closing of transactions at the time of completion of work on a request.
+
+Allows you to check for different database connections and for a variable number of events.
+By default, the component checks upon the Application::EVENT_AFTER_REQUEST event,
+as well as upon completion of the script, if it was stopped more rigidly.
+
+## Installation
+The preferred way to install this extension is through composer.
+
+Either run
+```shell script
+php composer.phar require --prefer-dist matrozov/yii2-transaction-checker "*"
+```
+
+or add
+```json
+"matrozov/yii2-transaction-checker": "*"
+```
+to the require section of your composer.json file.
+
+## Usage
+Specify the component and point it to the bootstrap section of the framework.
+```php
+'bootstrap' => ['transactionChecker'],
+'components' => [
+    'transactionChecker' => [
+        'class' => 'matrozov/yii2-transaction-checker/TransactionChecker',
+    ],
+],
+```
+
+You can directly specify the database components in which to check for
+transaction completion in the component configuration.
+By default, the component is "db".
+```php
+'connections' => ['db', 'db2'],
+```
+
+You can also use events in other components to control the completion of transactions in other cases. For example,
+if the processing of an event from the queue is completed.
+```php
+'extendedEvents' => [
+   'myComponent'  => 'final-work-event',
+   'myComponent2' => ['final-work-event-1', 'final-work-event-2'],
+],
+```
---
+++ src/TransactionChecker.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace chemexsol\common\components;
+
+use Yii;
+use yii\base\Application;
+use yii\base\Component;
+use yii\base\ErrorException;
+use yii\base\InvalidConfigException;
+use yii\db\Connection;
+
+/**
+ * Class TransactionChecker
+ *
+ * @package chemexsol\common\components
+ *
+ * @property string[]       $connections
+ * The names of the database components where you want to track the completion of the transaction.
+ * 'connections' => [
+ *    'db', 'db2'
+ * ],
+ * Default: ['db']
+ *
+ * @property string[string] $extendedEvents
+ * List of components and their events for which you want to track the completion of transactions.
+ *
+ * 'extendedEvents' => [
+ *    'myComponent'  => 'final-work-event',
+ *    'myComponent2' => ['final-work-event-1', 'final-work-event-2'],
+ * ],
+ * Default: []
+ */
+class TransactionChecker extends Component
+{
+    public $connections    = ['db'];
+    public $extendedEvents = [];
+
+    /**
+     * @inheritDoc
+     *
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+
+        Yii::$app->on(Application::EVENT_AFTER_REQUEST, [$this, 'checkTransaction'], 'Application::EVENT_AFTER_REQUEST');
+
+        register_shutdown_function([$this, 'checkTransaction'], 'register_shutdown_function');
+
+        foreach ($this->extendedEvents as $componentId => $eventNames) {
+            /** @var Component $component */
+            $component = Yii::$app->get($componentId);
+
+            $eventNames = is_array($eventNames) ? $eventNames : [$eventNames];
+
+            foreach ($eventNames as $eventName) {
+                $component->on($eventName, [$this, 'checkTransaction'], $componentId . '::' . $eventName);
+            }
+        }
+    }
+
+    /**
+     * @param string $eventName
+     *
+     * @throws ErrorException
+     * @throws InvalidConfigException
+     */
+    public function checkTransaction($eventName)
+    {
+        $connectionErrorIds = [];
+
+        foreach ($this->connections as $connectionId) {
+            /** @var Connection $db */
+            $db = Yii::$app->get($connectionId);
+
+            $transaction = $db->getTransaction();
+
+            if (!$db || !$transaction) {
+                continue;
+            }
+
+            $transaction->rollBack();
+
+            $connectionErrorIds[] = $connectionId;
+        }
+
+        if (!empty($connectionErrorIds)) {
+            $connectionErrorIds = implode(', ', $connectionErrorIds);
+            throw new ErrorException("Trigger \"$eventName\": Transaction in \"$connectionErrorIds\" does\'t closed!");
+        }
+    }
+}
